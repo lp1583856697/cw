99eecfaedc2c3a68ba9b89b21b200abc59b93c85
lp1583856697 <lp1583856697@163.com>
2019-10-14 20:53:46 +0800

    ...

---
+++ petlife_app/src/components/common/Carttop.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h3>购物车</h3>
+    <div class="topbg">
+      <img src="../../assets/empcar.png" alt="">
+      <p id="p1">购物车空空如也</p>
+      <p>再忙，也要记得买点什么犒赏您的爱宠</p>
+      <a href="">
+        <mt-buton>去逛逛</mt-buton>
+      </a>
+    </div>
+  </div>
+</template>
