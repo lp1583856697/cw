5f5589257ffc05480f12ae0f674626fa0dcae477
wm <wm@163.com>
2019-10-14 19:54:03 +0800

    mm

--- petlife_app/src/router/index.js
+++ petlife_app/src/router/index.js
@@ -2,14 +2,14 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
+// 1.进入组件XXX.vue
+// 将vue文件引入当前router下的index.js
+import Me from "../components/Me.vue"
+// 2.为组件指定访问路径
 Vue.use(Router)
-
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+    {path: '/Me',component: Me},
+    {path: '/',component: HelloWorld}
   ]
 })
