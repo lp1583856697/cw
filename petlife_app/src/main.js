3afa2346405bf6baef9d353e6650e97a56ce4801
DongQiannan <18291867058@163.com>
2019-10-14 19:54:17 +0800

    views

--- petlife_app/src/main.js
+++ petlife_app/src/main.js
@@ -3,7 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+//引入Mint-UI组件库
+import MintUI from "mint-ui"
+Vue.use(MintUI)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
