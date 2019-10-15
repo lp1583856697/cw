0fbe779fe1517ac4fc078be6a94306085cd6fae9
DongQiannan <18291867058@163.com>
2019-10-15 09:56:15 +0800

    qq

---
+++ petlife_app/src/components/views/Cart.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <carttop></carttop>
+  </div>
+</template>
+<script>
+import Carttop from '../common/Carttop.vue'
+export default {
+  data(){
+    return{}
+  },
+  components:{
+    "carttop":Carttop
+  }
+}
+</script>
