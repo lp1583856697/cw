6bca6c29d234c78beb1bf80e12acf40b039e30b3
DongQiannan <18291867058@163.com>
2019-10-15 19:05:42 +0800

    w

--- petlife_app/src/components/common/Carttop.vue
+++ petlife_app/src/components/common/Carttop.vue
@@ -1,13 +1,21 @@
+<!--购物车顶部-->
 <template>
-  <div>
+  <div class="container">
     <h3>购物车</h3>
     <div class="topbg">
       <img src="../../assets/empcar.png" alt="">
       <p id="p1">购物车空空如也</p>
       <p>再忙，也要记得买点什么犒赏您的爱宠</p>
       <a href="">
-        <mt-buton>去逛逛</mt-buton>
+        <mt-button>去逛逛</mt-button>
       </a>
     </div>
   </div>
 </template>
+<style scoped>
+  .topbg{
+    display: flex;
+    background:#ffd028;
+    border-radius:40%;
+    }
+</style>
