7a259b20035480d8f59d243dfe679caf63470f75
wm <wm@163.com>
2019-10-15 10:27:27 +0800

    mm-Y

--- petlife_app/src/router/index.js
+++ petlife_app/src/router/index.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+//1.功能:引入组件XXX.vue
+//将vue文件引入当前router下的index.js
 import Cart from '../components/views/Cart.vue'
 import Community from "../components/views/Community.vue"
 
 import Shopping from "../components/common/Shopping.vue"
 
+//2.为组件指定访问路径
 Vue.use(Router)
-
 export default new Router({
   routes: [
     {path:"/Shopping",component:Shopping},
