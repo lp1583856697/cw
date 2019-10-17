a42811dcc36350ecf7227f497fd2b6cb8e8c82fd
wm <wm@163.com>
2019-10-17 09:03:41 +0800

    mm-Y

--- petlife_app/src/components/views/Login.vue
+++ petlife_app/src/components/views/Login.vue
@@ -2,20 +2,17 @@
 <!-- 登录页面 -->
   <div>
     <mylogin1></mylogin1>
-    <mylogin2></mylogin2>
   </div>
 </template>
 <script>
 // 引入子组件
 import Mylogin1 from "../common/Mylogin1"
-import Mylogin2 from "../common/Mylogin2"
 export default {
   data(){
     return{}
   },
   components:{
-    "mylogin1":Mylogin1,
-    "mylogin2":Mylogin2,
+    "mylogin1":Mylogin1
   }
 }
 </script>
