d141eb2aa0f9766d77b0c9a6712fe747d2d09999
wm <wm@163.com>
2019-10-19 11:56:27 +0800

    mm-Y

--- petlife_app/src/components/common/Mylogin2.vue
+++ petlife_app/src/components/common/Mylogin2.vue
@@ -6,11 +6,21 @@
       <van-field v-model="phone"  :attr="{autofocus:true}" required clearable label="手机号" placeholder="请输入您的手机号" />
       <van-field v-model="upwd" type="password" label="密码" placeholder="请输入登录密码" required />
     </van-cell-group>
-    <div class="floor1">
+    <div class="d1">
       <span>忘记密码?</span>
       <span>注册账户</span>
     </div>
     <mt-button size="large" @click="login">登录</mt-button>
+    <p>
+      <img src="../../assets/img14.png" style="width:16px;height:16px;" alt="">
+      手机验证码登录
+    </p>
+    <div class="d2">更多登录方式</div>
+    <div class="d3">
+      <img src="../../assets/wx.png" style="width:40px;height:40px;" alt="">
+      <img src="../../assets/qq.png" style="width:40px;height:40px;" alt="">
+      <img src="../../assets/zfb.png" style="width:40px;height:40px;" alt="">
+    </div>
   </div>
 </template>
 <script>
@@ -62,7 +72,7 @@ export default {
     justify-content:space-between;
     flex-direction: column;
     align-items: center;
-    top:65px;
+    top:70px;
     margin:0 40px;
   }
   .v1{
@@ -74,27 +84,51 @@ export default {
     width: 70px;
     height: 70px;
     border-radius: 100%;
-    margin-bottom: 35px;
+    margin-bottom: 40px;
   }
   .van-cell{
-    font-size: 14px;
+    font-size: 16px;
     letter-spacing: 2px;
-    line-height: 28px;
+    line-height: 30px;
   }
-  .floor1{
+  .d1{
     display: flex;
     justify-content: space-between;
-    margin: 20px 35px 20px 35px;
+    margin: 25px 35px 25px 35px;
   }
   span{
     color: #999;
-    font-size: 14px;
+    font-size: 16px;
   }
   .mint-button{
     color:#fff;
-    background-color: #ccc;
+    background-color: #ddd;
     letter-spacing: 2px;
+    border-radius: 10px;
     font-weight: 600;
     height: 45px;
+  }
+  p{
+    font-size: 16px;
+    color:#000;
+    letter-spacing: 1px;
+    margin-top:25px;
+    font-weight: 500;
+  }
+  .d2{
+    display: flex;
+    justify-content: center;
+    margin: 60px auto 25px;
+    border-radius: 15px;
+    background-color: #eee;
+    font-size: 13px;
+    color:#000;
+    width: 130px;
+    height: 24px;
+    line-height: 24px;
+  }
+  .d3{
+    display: flex;
+    justify-content: space-around;
   }
 </style>
